24f1708cf15cdc685c30771e089b273fcc37feb9
raemilcf <raemilcorniel@hotmail.com>
2023-04-22 19:00:12 -0400

    add scss, add json with images

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './categories.styles.scss'
 
 const App  = ()=> {
 
-
   const categories = [
     {
-      id: 1,
-      title: 'Hats'
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: 2,
-      title: 'Sneakers'
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
-
+  
   return (
     <div className="categories-container">
-    {categories.map((category) => {
+    {categories.map(({id, title, imageUrl}) => {
           return (
-            <div key={category.id}  class="categories-contariner">
-              <div className='background-image'/>
-                <div className='categories-body-container'>
-                  <h2>{category.title}</h2>
-                  <p>shop now</p>
+            <div key={id}  class="category-container">
+              <div className='background-image' style={{
+                backgroundImage: `url(${imageUrl})`
+              }}/>
+              <div className='category-body-container'>
+                <h2>{title}</h2>
+                <p>shop now</p>
+              </div>
 
-                </div>
+               
             </div>   
           );
         }
       )
-    };
+    }
     </div>  
   );
   
