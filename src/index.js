45be1da944309244c8e22a0caa351cdc19e80ca3
Raemil Corniel Francisco <r.corniel@bravo.com.do>
2023-04-26 15:55:50 -0400

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'; //add react-router-dom to handle navigation 
+import { BrowserRouter , HashRouter as Router} from 'react-router-dom'; //add react-router-dom to handle navigation 
 
 
 import App from './App';
@@ -15,19 +15,22 @@ import './index.scss';
 //before inserting the provider we need to identify if is needed after login or before any other provider
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  <BrowserRouter>
-  {/* which user has access to our components  */}
-  <UserProvider> 
-    <ProductsProvider>  
-      <CartProvider>
-        
-        <App />
-      </CartProvider>
-    </ProductsProvider>
-    </UserProvider> 
-  </BrowserRouter>
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <BrowserRouter>
+      {/* which user has access to our components  */}
+      <UserProvider> 
+        <ProductsProvider>  
+          <CartProvider>
+            
+            <App />
+          </CartProvider>
+        </ProductsProvider>
+        </UserProvider> 
+      </BrowserRouter>
+    </React.StrictMode>
+  </Router>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
